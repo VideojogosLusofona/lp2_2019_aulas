ab1266310eb7e76c840c7311e5d6e59508533d8f
Nuno Fachada <faken@fakenmc.com>
2019-10-27 23:20:07 +0000

    Adicionar ficheiros iniciais para aula 7

---
+++ Aula07/EventTest/Assets/Scripts/EventMaster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMaster : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey) OnKeyPress();
+    }
+
+    private void OnGUI()
+    {
+        GUI.color = Color.blue;
+        GUI.Label(new Rect(10, 10, 500, 20), "Press S to shoot.");
+    }
+
+    protected virtual void OnKeyPress()
+    {
+        if (KeyPress != null) KeyPress();
+    }
+
+    public event Action KeyPress;
+
+}
