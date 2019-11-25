b07edeb37059a380188fda90a2af115e99d53c9f
Nuno Fachada <nuno.fachada@ulusofona.pt>
2019-11-25 11:43:53 +0000

    Adicionar exemplo para aula 9

---
+++ Aula09/Threads/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(CountTo250);
+            thread1.Start();
+            Thread thread2 = new Thread(CountTo250);
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.ReadKey();
+        }
+
+        public static void CountTo250()
+        {
+            for (int index = 0; index < 250; index++)
+                Console.WriteLine(index + 1);
+        }
+    }
+}
